58c873878a64f053252d08f9eb741f09e1a7419d
Jarad Niemi <niemi@iastate.edu>
2014-06-30 13:06:50 -0500

    updated function with roxygen comments

--- R/example_function.R
+++ R/example_function.R
@@ -1,3 +1,14 @@
+#' An example function that sums two numbers
+#' 
+#' @param a a number
+#' @param b a number
+#' @return a numeric vector giving the component-wise sum.
+#' @seealso '+'
+#' @export
+#' @examples
+#' example_function(1,1)
+#' example_function(pi,exp(1))
+#' example_function(1:2,3:4)
 example_function = function(a,b) {
   a+b
 }
