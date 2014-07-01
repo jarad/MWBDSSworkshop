036c1d356b0890129531725aee897ac746792ba7
Jarad Niemi <niemi@iastate.edu>
2014-07-01 12:10:23 -0500

    added roxygen help file for GI dataset

---
+++ data/GI.R
@@ -0,0 +1,19 @@
+#' Fictional syndromic surveillance data set
+#' 
+#' A data set containing fictitious patient data for gastrointestinal (GI)
+#' visits to an emergency department (ER). 
+#' 
+#' \itemize{
+#'   \item id. patient id
+#'   \item date. date of visit to ER
+#'   \item facility. facility id
+#'   \item icd9. international statistical classificiation of diseases, ninth revision, code
+#'   \item age. patient age
+#'   \item zipcode. patient zip code
+#'   \item chief_complain. free text chief complaint
+#'   \item syndrome. syndromic surveillance code 
+#' }
+#' 
+#' @format A data frame with 21244 rows and 8 columns
+#' @name GI
+NULL
