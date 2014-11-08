a546d35caee5695f1c9ea8529d0727552d766aa9
Jarad Niemi <niemi@iastate.edu>
2014-11-08 10:12:18 -0600

    add inst/doc

---
+++ inst/doc/calculator-activity.R
@@ -0,0 +1,7 @@
+## ------------------------------------------------------------------------
+specificity = 0.99
+sensitivity = 0.95
+prevalence = 0.001
+probability = (specificity*prevalence) / (specificity*prevalence + (1-sensitivity)*(1-prevalence))
+probability
+
---
+++ inst/doc/index.R
@@ -0,0 +1,4 @@
+## ----setup, include=FALSE------------------------------------------------
+library(knitr)
+opts_chunk$set(cache=TRUE, cache.path = 'DocumentName_cache/', fig.path='figure/')
+
