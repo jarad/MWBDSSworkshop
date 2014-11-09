49d4fd051beb5c609282cdf7053fd549675d541a
Jarad Niemi <niemi@iastate.edu>
2014-11-09 08:16:40 -0600

    added package help file

--- R/workshop.R
+++ R/workshop.R
@@ -3,9 +3,9 @@
 #'  @param create_data logical. If TRUE, writes the GI data set to a csv file.
 #'  @param clean local. If TRUE, removes all objects created by the function from the R workspace.
 #'  @param launch_index. If TRUE, launches the workshop html index.
-#'  @seealso The function uses the 'data' function to load the 'GI' data set and 
-#'    then uses the 'write.csv' function to write it to a file. It then launches the workshop html index 
-#'    using the 'vignete' function. 
+#'  @seealso The function uses the \code{\link{data}} function to load the \code{\link{GI}} data set and 
+#'    then uses the \code{\link{write.csv}} function to write it to a file. It then launches the workshop html index 
+#'    using the \code{\link{vignette}} function. 
 #'  @author Jarad Niemi <\url{http://jarad.me}>
 #'  @export
 #'  @examples
