83e024b9d629f3336288c7eed0341c28f6b99c8f
Jarad Niemi <niemi@iastate.edu>
2014-11-09 08:03:51 -0600

    function to start workshop

---
+++ R/workshop.R
@@ -0,0 +1,24 @@
+#'  A function that prepares the workhsop
+#' 
+#'  @param create_data logical. If TRUE, writes the GI data set to a csv file.
+#'  @param clean local. If TRUE, removes all objects created by the function from the R workspace.
+#'  @param launch_index. If TRUE, launches the workshop html index.
+#'  @seealso The function uses the 'data' function to load the 'GI' data set and 
+#'    then uses the 'write.csv' function to write it to a file. It then launches the workshop html index 
+#'    using the 'vignete' function. 
+#'  @author Jarad Niemi <\url{http://jarad.me}>
+#'  @export
+#'  @examples
+#'  workshop()
+#'  workshop(write_data = FALSE)
+#'  workshop(clean = FALSE) 
+workshop = function(write_data = TRUE, clean = TRUE, launch_index=TRUE) {
+  
+  if (write_data) {
+    data(GI)
+    write.csv(GI, file="GI.csv", row.names=FALSE)
+    if (clean) rm(GI)
+  }
+  
+  vignette('workshop')
+}
