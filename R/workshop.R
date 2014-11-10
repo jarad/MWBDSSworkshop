5e925f7749940703976b6973328bb74ecc8b50fe
Jarad Niemi <niemi@iastate.edu>
2014-11-09 19:50:53 -0600

    added reading data into R

--- R/workshop.R
+++ R/workshop.R
@@ -3,10 +3,11 @@
 #'  @param write_data logical. If TRUE, writes the GI data set to a csv file.
 #'  @param clean logical. If TRUE, removes all objects created by the function from the R workspace.
 #'  @param launch_index logical. If TRUE, launches the workshop html index.
-#'  @seealso The function uses the \code{\link{data}} function to load the \code{\link{GI}} data set and 
+#'  @details The function uses the \code{\link{data}} function to load the \code{\link{GI}} data set and 
 #'    then uses the \code{\link{write.csv}} function to write it to a file. 
 #'    It then launches the \code{\link{ISDSWorkshop}} html index 
 #'    using the \code{\link{vignette}} function. 
+#'  @seealso \code{\link{data}},\code{\link{GI}},\code{\link{write.csv}},\code{\link{ISDSWorkshop}}
 #'  @author Jarad Niemi <\url{http://jarad.me}>
 #'  @export
 #'  @examples
@@ -16,12 +17,10 @@
 #'  workshop(clean = FALSE) 
 #'  }
 workshop = function(write_data = TRUE, clean = TRUE, launch_index=TRUE) {
-  GI = NULL # To appease R CMD check
-  
   if (write_data) {
-    data(GI)
+    data('GI', package='ISDSWorkshop')
     write.csv(GI, file="GI.csv", row.names=FALSE)
-    if (clean) rm(GI)
+    if (clean) rm('GI')
   }
   
   vignette('workshop')
