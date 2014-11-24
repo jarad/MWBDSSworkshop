a367163db379d83fad66485cb5f56dbe7bab1b23
Jarad Niemi <niemi@iastate.edu>
2014-11-24 13:07:27 -0600

    added icd9 documentation

---
+++ R/icd9.R
@@ -0,0 +1,16 @@
+#' ICD9 codes
+#' 
+#' A data set that contains the International Statistical Classification of Diseases 
+#' and Related Health Problems (ICD-9) codes and their classification.
+#' 
+#' @name icd9
+#' @docType data
+#' @author Jarad Niemi 
+#' \itemize{
+#'   \item code_cutpoint The lower end of the range for the ICD9 code representing that classification.
+#'   \item classification The ICD9 classification
+#' }
+#' @keywords datasets
+#' @usage data(icd9)
+#' @format A data frame with 19 rows and 2 columns
+NULL
