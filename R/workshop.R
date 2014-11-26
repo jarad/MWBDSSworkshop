2f8dae659ac6be556e963252bf0a89ff6b8ebdaa
Jarad Niemi <niemi@iastate.edu>
2014-11-26 11:26:42 -0600

    updated workshop function

--- R/workshop.R
+++ R/workshop.R
@@ -1,7 +1,7 @@
 #'  A function that prepares the workshop.
 #' 
-#'  @param write_data string. Write these data sets.
-#'  @param write_scripts string. write these data sets
+#'  @param write_data logical or string. If TRUE, writes all data sets. If FALSE, write no data sets. If string, vector of data sets to be written.
+#'  @param write_scripts logical or string. If TRUE, writes all scripts. If FALSE, write no scripts. If string, vector of scripts to be written.
 #'  @param launch_index logical. If TRUE, launches the workshop html index.
 #'  @details By default, the function uses the \code{\link{data}} function to load data sets and 
 #'    then uses the \code{\link{write.csv}} function to write them to a file. 
@@ -16,13 +16,12 @@
 #'  workshop(write_data = FALSE)
 #'  workshop(clean = FALSE) 
 #'  }
-workshop = function(write_data = 'all', 
-                    write_scripts='all', 
-                    launch_index=TRUE) {
+workshop = function(write_data    = TRUE, 
+                    write_scripts = TRUE, 
+                    launch_index  = TRUE) {
   
   # Write data
-  if (write_data == 'all') 
-    write_data = c('GI','icd9','fluTrends')
+  if (write_data) write_data = c('GI','icd9','fluTrends')
   
   if ("GI" %in% write_data) {
     data('GI', package='ISDSWorkshop', envir=environment())
@@ -39,12 +38,14 @@ workshop = function(write_data = 'all',
   if ("fluTrends" %in% write_data) {
     data('fluTrends', package='ISDSWorkshop', envir=environment())
     write.csv(get('fluTrends', envir=environment()),
-              file = 'fluTrends.csv', envir=environment())
+              file = 'fluTrends.csv', row.names=FALSE)
   }
   
   # Write scripts
-  if (write_scripts == 'all') 
-    write_scripts = c("intro","graphics","advanced_graphics","biosurveillance")
+  if (write_scripts) write_scripts = c("intro",
+                                       "graphics",
+                                       "advanced_graphics",
+                                       "biosurveillance")
   
   for (script in write_scripts) 
     file.copy(from = paste(find.package("ISDSWorkshop"), "/doc/", script, ".R", sep=""),
