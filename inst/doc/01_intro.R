53eabfbbb1c1551187fa93177880b30e01258d11
jarad <niemi@iastate.edu>
2016-11-28 20:51:22 -0600

    Add vignette to inst/doc

---
+++ inst/doc/01_intro.R
@@ -0,0 +1,244 @@
+## ---- eval=FALSE---------------------------------------------------------
+#  help.start()
+
+## ---- eval=FALSE---------------------------------------------------------
+#  >
+
+## ---- eval=FALSE---------------------------------------------------------
+#  help()
+
+## ---- eval=FALSE---------------------------------------------------------
+#  hepl()
+#  help() # Use up arrow and fix
+
+## ------------------------------------------------------------------------
+a = 1 
+b = 2
+a+b
+
+## ------------------------------------------------------------------------
+x = 1:10
+y = rep(c(1,2), each=5)
+m = lm(y~x)
+s = summary(m)
+
+## ---- eval=FALSE---------------------------------------------------------
+#  x
+#  y
+#  m
+#  s
+#  s$r.squared
+
+## ------------------------------------------------------------------------
+1+2
+1-2
+1/2
+1*2
+
+## ------------------------------------------------------------------------
+(1+3)*2 + 100^2  # standard order of operations
+sin(2*pi)        # the result is in scientific notation, i.e. -2.449294 x 10^-16 
+sqrt(4)
+10^2
+log(10)          # the default is base e
+log(10, base=10)
+
+## ------------------------------------------------------------------------
+a = 1
+b = 2
+a+b
+a-b
+a/b
+a*b
+
+## ------------------------------------------------------------------------
+a <- 1
+2 -> b
+c = 3  # is the same as <-
+
+## ------------------------------------------------------------------------
+a
+b
+c
+
+## ------------------------------------------------------------------------
+population = 1000
+number_infected = 200
+deaths = 3
+
+death_rate = deaths / number_infected
+attack_rate = number_infected / population
+
+death_rate
+attack_rate
+
+## ---- echo=FALSE---------------------------------------------------------
+# Find the probability the individual has the disease if 
+# specificity is 0.99, sensitivity is 0.95, and prevalence is 0.001
+
+## ---- eval=FALSE---------------------------------------------------------
+#  setwd(choose.dir(getwd()))
+
+## ---- eval=FALSE---------------------------------------------------------
+#  install.packages('plyr')
+
+## ---- eval=FALSE---------------------------------------------------------
+#  library('dplyr')
+
+## ---- warning=FALSE------------------------------------------------------
+library('ISDSWorkshop')
+
+## ---- warning=FALSE------------------------------------------------------
+workshop()
+
+## ------------------------------------------------------------------------
+# This is just a comment. 
+1+1 # So is this
+# 1+2
+
+## ------------------------------------------------------------------------
+GI = read.csv("GI.csv")
+
+## ---- tidy=FALSE---------------------------------------------------------
+GI2 = read.table("GI.csv", 
+                 header=TRUE, # There is a header.
+                 sep=",")     # The column delimiter is a comma.
+
+## ------------------------------------------------------------------------
+all.equal(GI, GI2)
+
+## ---- eval=FALSE---------------------------------------------------------
+#  install.packages('xlsx')
+#  library('xlsx')
+#  d = read.xlsx("filename.xlsx", sheetIndex=1) # or
+#  d = read.xlsx("filename.xlsx", sheetName="sheetName")
+
+## ---- eval=FALSE---------------------------------------------------------
+#  install.packages('haven')
+#  library('haven')
+#  d = read_sas('filename.sas7bdat)
+
+## ------------------------------------------------------------------------
+dim(GI)
+nrow(GI)
+ncol(GI)
+names(GI) # column names
+head(GI, n=5) # first 5 rows of the data.frame
+tail(GI, n=5) # last5 rows of the data.frame
+
+## ---- eval=FALSE, tidy=FALSE---------------------------------------------
+#  d = read.table("filename.csv", # Make sure to change filename>to your filename and
+#                                 # make sure you use the extension, e.g. .csv.
+#                 header = TRUE,  # If there is no header column, change TRUE to FALSE.
+#                 sep =",",       # The column delimiter is a comma.
+#                 skip = 0        # Skip this many lines before starting to read the file
+#                 )
+
+## ---- echo=FALSE---------------------------------------------------------
+# Read in the csv file
+
+## ------------------------------------------------------------------------
+summary(GI)
+
+## ---- eval=FALSE---------------------------------------------------------
+#  GI$age     # or
+#  GI[,'age'] # or
+#  GI[,5]     # since age is the 5th column
+
+## ------------------------------------------------------------------------
+min(GI$age)
+max(GI$age)
+mean(GI$age)
+median(GI$age)
+quantile(GI$age, c(.025,.25,.5,.75,.975))
+summary(GI$age)
+
+## ------------------------------------------------------------------------
+table(GI$gender)
+
+## ------------------------------------------------------------------------
+summary(GI$gender)
+
+## ------------------------------------------------------------------------
+table(GI$facility)
+summary(GI$facility)
+
+## ------------------------------------------------------------------------
+library('dplyr')
+
+GI_37 <- GI %>% 
+  filter(facility==37) # Notice the double equal sign!
+
+nrow(GI_37)            # Number of rows (observations) in the new data set
+
+## ------------------------------------------------------------------------
+GI_AbdPain <- GI %>% 
+  filter(chief_complaint == "Abd Pain") # Need to quote non-numeric variable level
+
+nrow(GI_AbdPain)
+
+## ------------------------------------------------------------------------
+GI_37a = GI[GI$facility==37,]
+all.equal(GI_37, GI_37a)
+
+GI_37b = subset(GI, facility==37)
+all.equal(GI_37, GI_37b)
+
+
+GI_AbdPain1 = GI[GI$chief_complaint == "Abd Pain",]
+all.equal(GI_AbdPain, GI_AbdPain1)
+
+GI_AbdPain2 = subset(GI, chief_complaint == "Abd Pain")
+all.equal(GI_AbdPain, GI_AbdPain2)
+
+## ---- eval=FALSE---------------------------------------------------------
+#  GI %>% filter(age <   5)
+#  GI %>% filter(age >= 60)
+#  GI %>% filter(chief_complaint %in% c("Abd Pain","ABD PAIN")) # Abd Pain or ABD PAIN
+#  GI %>% filter(tolower(chief_complaint) == "abd pain")        # any capitalization pattern
+#  GI %>% filter(!(facility %in% c(37,66)))                     # facility is NOT 37 or 66
+
+## ------------------------------------------------------------------------
+summary(GI_37$age)
+summary(GI_AbdPain$age)
+
+## ---- echo=FALSE---------------------------------------------------------
+# Find the min, max, mean, and median age for zipcode 20032.
+
+## ------------------------------------------------------------------------
+hist(GI$age)
+
+## ------------------------------------------------------------------------
+hist(GI$age, 50)
+
+## ------------------------------------------------------------------------
+hist(GI$age, 0:158)
+
+## ------------------------------------------------------------------------
+boxplot(GI$age)
+
+## ------------------------------------------------------------------------
+boxplot(age~facility, GI, xlab="Facility", ylab="Age")
+
+## ------------------------------------------------------------------------
+GI$date = as.Date(GI$date)
+plot(age~date, GI)
+
+## ------------------------------------------------------------------------
+counts = table(GI$facility)
+barplot(counts, xlab="Facility", ylab="Count", main="Number of observations at each facility")
+
+## ---- echo=FALSE---------------------------------------------------------
+# Construct a histogram for age at facility 37.
+
+# Construct a boxplot for age at facility 37. 
+
+## ---- echo=FALSE---------------------------------------------------------
+# Construct a bar chart for the zipcode at facility 37.
+
+## ---- eval=FALSE---------------------------------------------------------
+#  ?mean
+
+## ---- eval=FALSE---------------------------------------------------------
+#  help.search("mean")
+
---
+++ inst/doc/04_biosurveillance.R
@@ -0,0 +1,230 @@
+## ---- message=FALSE------------------------------------------------------
+library('ggplot2')
+library('dplyr')
+library('tidyr')
+library('RColorBrewer') 
+library('ISDSWorkshop')
+workshop(launch_index=FALSE)
+
+## ------------------------------------------------------------------------
+# Read csv files and create additional columns
+icd9df = read.csv("icd9.csv")
+GI     = read.csv("GI.csv") %>%
+  mutate(
+    date      = as.Date(date),
+    weekC     = cut(date, breaks="weeks"),
+    week      = as.numeric(weekC),
+    weekD     = as.Date(weekC),
+    facility  = as.factor(facility),
+    icd9class = factor(cut(icd9, 
+                           breaks = icd9df$code_cutpoint, 
+                           labels = icd9df$classification[-nrow(icd9df)], 
+                           right  = TRUE)),
+    ageC      = cut(age, 
+                    breaks = c(-Inf, 5, 18, 45 ,60, Inf)),
+    zip3      = trunc(zipcode/100))
+
+## ------------------------------------------------------------------------
+ga_l <- GI %>%
+  group_by(gender, ageC) %>%
+  summarize(count = n())
+
+ga_w <- ga_l %>%
+  spread(ageC, count) %>%
+  print(row.names = FALSE)
+
+## ------------------------------------------------------------------------
+library('xtable')
+tab = xtable(ga_w,
+             caption = "Total GI cases by Sex and Age Category",
+             label   = "myHTMLanchor",
+             align   = "ll|rrrrr") # rownames gets a column
+
+## ------------------------------------------------------------------------
+print(tab, file="table.html", type="html", include.rownames=FALSE)
+
+## ------------------------------------------------------------------------
+print(tab, type="html", include.rownames=FALSE)
+
+## ---- results='asis'-----------------------------------------------------
+print(tab, type="html", include.rownames=FALSE)
+
+## ---- eval=FALSE---------------------------------------------------------
+#  # Summarize data by facility and age category
+#  
+#  # Reshape data from long to wide format
+#  
+#  # Create HTML table
+#  
+#  # Save HTML to file
+#  
+#  # Copy-and-paste table into Word
+
+## ------------------------------------------------------------------------
+library('maps')
+states = map_data("state")
+ggplot(states, aes(x = long, y = lat, group = group)) + 
+  geom_polygon(color="white")
+
+## ------------------------------------------------------------------------
+counties = map_data("county")
+ggplot(counties, aes(x = long, y = lat, group = group)) + 
+  geom_polygon(color = "white")
+
+## ------------------------------------------------------------------------
+ggplot(counties %>% filter(region=="iowa"), 
+       aes(x = long, y = lat, group = group)) + 
+  geom_polygon(fill = "gray", color = "black")
+
+## ------------------------------------------------------------------------
+fluTrends = read.csv("fluTrends.csv", check.names=FALSE)
+
+## ------------------------------------------------------------------------
+nr = nrow(fluTrends)
+flu_w = fluTrends[(nr-11):nr, c(1,3:53)]
+dim(flu_w)
+
+## ------------------------------------------------------------------------
+flu_l <- flu_w %>%
+  gather(region, index, -Date)
+
+## ------------------------------------------------------------------------
+head(unique(states$region))
+flu_l$region = tolower(flu_l$region)
+states_merged = merge(states, flu_l, sort=FALSE, by='region')
+
+## ------------------------------------------------------------------------
+states_merged$Date = as.Date(states_merged$Date)
+mx_date = max(states_merged$Date)
+ggplot(states_merged %>% filter(Date == mx_date), 
+       aes(x = long, y = lat, group = group, fill = index)) + 
+  geom_polygon() + 
+  labs(title=paste('Google Flu Trends on', mx_date), x='', y='') +
+  theme_minimal() + 
+  theme(legend.title = element_blank()) +
+  coord_map("cylindrical") + 
+  scale_fill_gradientn(colours=brewer.pal(9,"Reds")) # Thanks Annie Chen
+
+## ------------------------------------------------------------------------
+ggplot(states_merged, 
+       aes(x=long, y=lat, group=group, fill=index)) + 
+  geom_polygon() + 
+  labs(title='Google Flu Trends', x='', y='') +
+  theme_minimal() + 
+  theme(legend.title = element_blank()) +
+  facet_wrap(~Date) + 
+  coord_map("cylindrical") + 
+  scale_fill_gradientn(colours=brewer.pal(9,"Reds")) 
+
+## ---- eval=FALSE---------------------------------------------------------
+#  # Construct Google Flu Trends map
+
+## ---- eval=FALSE---------------------------------------------------------
+#  install.packages("ggplot2")
+
+## ---- eval=FALSE---------------------------------------------------------
+#  # install.packages("devtools")
+#  library('devtools')
+#  install_github('nandadorea/vetsyn')
+
+## ---- eval=FALSE---------------------------------------------------------
+#  source("http://bioconductor.org/biocLite.R")
+#  biocLite()
+
+## ---- eval=FALSE---------------------------------------------------------
+#  biocLite("edgeR")
+
+## ---- eval=FALSE---------------------------------------------------------
+#  install.packages("ISDSWorkshop_0.2.tar.gz",
+#                   repos = NULL,
+#                   type  = "source")
+
+## ------------------------------------------------------------------------
+library('SpatialEpi')
+
+## Load Pennsylvania Lung Cancer Data
+data(pennLC)
+data <- pennLC$data
+
+## Process geographical information and convert to grid
+geo <- pennLC$geo[,2:3]
+geo <- latlong2grid(geo)
+
+## Get aggregated counts of population and cases for each county
+population <- tapply(data$population, data$county, sum)
+cases      <- tapply(data$cases,      data$county, sum)
+
+## Based on the 16 strata levels, compute expected numbers of disease
+n.strata       <- 16
+expected.cases <- expected(data$population, data$cases, n.strata)
+
+## Set Parameters
+pop.upper.bound <- 0.5
+n.simulations   <- 999
+alpha.level     <- 0.05
+plot            <- TRUE
+
+## ------------------------------------------------------------------------
+clr = cases/population
+clr = 1-clr/max(clr) # make sure range is (0,1)
+plot(pennLC$spatial.polygon, axes=TRUE, col=rgb(1, clr, clr))
+
+## ------------------------------------------------------------------------
+## Kulldorff using Binomial likelihoods
+binomial <- kulldorff(geo, cases, population, NULL, pop.upper.bound, n.simulations, 
+  alpha.level, plot)
+cluster <- binomial$most.likely.cluster$location.IDs.included
+
+## plot
+plot(pennLC$spatial.polygon,axes=TRUE)
+plot(pennLC$spatial.polygon[cluster],add=TRUE,col="red")
+title("Most Likely Cluster")
+
+## ---- eval=FALSE---------------------------------------------------------
+#  # Install the surveillance package
+
+## ------------------------------------------------------------------------
+add = function(a,b) {
+  return(a+b)
+}
+add(1,2)
+
+## ------------------------------------------------------------------------
+add_vector = function(v) {
+  sum = 0
+  for (i in 1:length(v)) sum = sum + v[i]
+  return(sum)
+}
+
+## ------------------------------------------------------------------------
+alert = function(y,threshold=100) {
+  # y is the time series 
+  # an alert is issue for any y above the threshold (default is 100)
+  factor(ifelse(y>threshold, "Yes", "No"))
+}
+
+## ------------------------------------------------------------------------
+GI_w <- GI %>%
+  group_by(weekD) %>%
+  summarize(count = n())
+
+GI_w$alert = alert(y = GI_w$count, threshold = 150)
+
+ggplot(GI_w, aes(x = weekD, y = count, color = alert)) + 
+  geom_point() + 
+  scale_color_manual(values = c("black","red"))
+
+## ---- eval=FALSE---------------------------------------------------------
+#  # Read in the data perhaps as a csv file
+#  
+#  # Create some table and save them to html files
+#  
+#  # Create some figures and save them to jpegs
+#  
+#  # Run some outbreak detection algorithms and produce figures
+
+## ---- eval=FALSE---------------------------------------------------------
+#  install.packages('shiny')
+#  library(shiny)
+#  runGitHub('NLMichaud/WeeklyCDCPlot')
+
