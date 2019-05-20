62602078f1b8126543d824faddcbeced31f882ae
Jarad Niemi <niemi@iastate.edu>
2019-05-20 17:12:21 -0500

    Use different file for fluTrends data

--- data-raw/fluTrends.R
+++ data-raw/fluTrends.R
@@ -1,6 +1,6 @@
-# https://www.google.org/flutrends/about/data/flu/historic/us-historic-v3.txt
+# https://www.google.org/flutrends/about/data/flu/historic/us-historic-v2.txt
 
-fluTrends = readr::read_csv("us-historic-v3.txt",
-                            skip = 15)
+fluTrends = readr::read_csv("us-historic-v2.txt",
+                            skip = 8)[,c(1,3:53)]
 
 usethis::use_data(fluTrends, overwrite = TRUE)
